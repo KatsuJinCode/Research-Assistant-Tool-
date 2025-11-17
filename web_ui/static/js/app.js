bc0437254a12aa53d802d44294a50e7cc6f48dd4
KatsuJinCode <152846207+KatsuJinCode@users.noreply.github.com>
2025-11-17 01:45:18 -0800

    chore: auto-commit before response completion
    Changes: 1 files modified
     1 file changed, 9 insertions(+)
    
    Auto-committed by enforce-commit hook to prevent data loss.
    (See Nov 9th incident: 8+ hours of work lost due to uncommitted changes)

--- web_ui/static/js/app.js
+++ web_ui/static/js/app.js
@@ -103,13 +103,22 @@ const App = {
             if (uploadZone) {
                 uploadZone.addEventListener('click', (e) => {
                     console.log('Upload zone clicked!');
+                    console.log('Click target:', e.target);
+                    console.log('Target ID:', e.target.id);
+                    console.log('Is upload button?', e.target === uploadBtn);
+                    console.log('Is URL input?', e.target.id === 'url-input');
+
                     // Don't trigger if clicking the button or URL input
                     if (e.target === uploadBtn || e.target.id === 'url-input') {
                         console.log('Ignoring click on button/input within zone');
                         return;
                     }
+
+                    console.log('About to trigger file picker...');
                     e.preventDefault();
+                    e.stopPropagation();
                     fileInput.click();
+                    console.log('File picker triggered!');
                 });
             }
 
